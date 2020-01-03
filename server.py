486abc14f197785e46a30ca491849394924685fc
bing <bingliu212536@sohu-inc.com>
2020-01-03 18:40:46 +0800

    fix

--- server.py
+++ server.py
@@ -24,8 +24,8 @@ def index():
     # movie = get_list(0, 5, "movie")
     # yingxiao = get_list(0, 5, "yingxiao")
     # 以下改為多線程去拉
-    t1 = MyThread(get_list_rand, (0,6,"all"))
-    t2 = MyThread(get_list_rand, (0,6,"all"))
+    t1 = MyThread(get_list, (0,6,"all"))
+    t2 = MyThread(get_list, (0,6,"all"))
     t3 = MyThread(get_list, (0,5,"food"))
     t4 = MyThread(get_list, (0,5,"movie"))
     t5 = MyThread(get_list, (0,5,"yingxiao"))
